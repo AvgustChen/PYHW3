33866ce2aedee1728fe632f7284f79086fffd484
AvgustChen <avchen@yandex.ru>
2022-10-02 13:26:13 +0300

    all done

--- task6.py
+++ task6.py
@@ -2,11 +2,22 @@
 #Реализуйте алгоритм задания случайных чисел без использования встроенного генератора псевдослучайных чисел.
 import time
 
-def rondom (n2):
+def rondom (min, max, n2):
     list = []
+    num = 0
+    maximum = 0
+    if max <= 10:
+        maximum = 10
+    elif max <= 100:
+        maximum = 100
+    elif max <= 1000:
+        maximum = 1000
+
     for i in range (1, n2+1):
-        list.append(round(i * (time.time() * 1000)%100)) # Здесь процентом выбираешь двузначные, однозначные и т.д. Потом можно будет докрутить в пределах выбранных
+         num = (round(i * (time.time() * 1000)%maximum))
+         if num > min < max:
+            list.append(num) 
     return list
     
-list = rondom(8)
+list = rondom(1, 1000, 8)
 print(list)
